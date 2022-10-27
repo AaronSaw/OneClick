2d0e15cc17927052c9f36c7a4b569b50d051b7b1
tinhtarwai <tinhtarwai106330@gmail.com>
2022-10-27 16:35:41 +0630

    upload common design for admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin-dashboard', function () {
+    return view('layouts.admin_common');
 });
+
+
