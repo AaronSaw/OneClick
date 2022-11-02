3112b8255a18f95a558b7c4e904b7a2bc65f7920
AaronSaw <aaronsaw702@gmail.com>
2022-11-02 08:47:22 +0630

    categoryApi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,11 @@ class AppServiceProvider extends ServiceProvider
         //Business Logic Registeration
         $this->app->bind('App\Contracts\Services\Auth\AuthServiceInterface', 'App\Services\Auth\AuthService');
         $this->app->bind('App\Contracts\Dao\Category\CategoryDaoInterface', 'App\Dao\Category\CategoryDao');
+        $this->app->bind('App\Contracts\Dao\CategoryApi\CategoryApiDaoInterface', 'App\Dao\CategoryApi\CategoryApiDao');
 
         $this->app->bind('App\Contracts\Services\Product\ProductServiceInterface', 'App\Services\Product\ProductService');
         $this->app->bind('App\Contracts\Services\Category\CategoryServiceInterface', 'App\Services\Category\CategoryService');
-
+        $this->app->bind('App\Contracts\Services\CategoryApi\CategoryApiServiceInterface', 'App\Services\CategoryApi\CategoryApiService');
     }
 
     /**
