ead4b8875576292139f58b9c35b1b1d777fbe130
AaronSaw <aaronsaw702@gmail.com>
2022-11-09 16:57:55 +0630

    orderList

--- app/Dao/Order/OrderDao.php
+++ app/Dao/Order/OrderDao.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Dao\Order;
+
 use App\Models\Order;
 use App\Contracts\Dao\Order\OrderDaoInterface;
 use Illuminate\Http\Request;
@@ -10,16 +11,41 @@ use Illuminate\Http\Request;
  */
 class OrderDao implements OrderDaoInterface
 {
-  /**
-   * To show Orders list
-   */
-    public function getIndex() {
-        $orders = Order::paginate(5);
+    /**
+     * To show Orders list
+     */
+    public function getIndex()
+    {
+        $orders = Order::Join('users', 'users.id', '=', 'orders.user_id')
+            ->Join('products', 'products.id', '=', 'orders.product_id')
+            ->select('orders.*', 'users.name', 'users.email', 'users.address', 'products.title', 'products.price', 'products.category_id')
+            ->when(request('name'), function ($q) {
+                $name = request("name");
+                $q->where("users.name", "like", "%$name%");
+            })
+            ->when(request('title'), function ($q) {
+                $title = request("title");
+                $q->where("products.title", "like", "%$title%");
+            })
+            ->when(request('address'), function ($q) {
+                $address = request("address");
+                $q->where("users.address", "like", "%$address%");
+            })
+            ->when(request('sdate'), function ($p) {
+                $sDate = request("sdate");
+                $p->where("orders.created_at", ">", "$sDate");
+            })
+            ->when(request('edate'), function ($e) {
+                $eDate = request("edate");
+                $e->where("orders.created_at", "<", "$eDate");
+            })
+            ->latest('id')
+            ->paginate(5)->withQueryString();
         return $orders;
     }
 
-    public function deleteOrder($id) {
-        $order = Order::findOrfail($id);
-        $order->delete();
+    public function deleteOrder($id)
+    {
+        $id->delete();
     }
 }
